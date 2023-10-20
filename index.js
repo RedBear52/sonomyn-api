7c0686a016273fb7b0a9e771e54c22960010cfd3
Ryan Spearman <94654287+RedBear52@users.noreply.github.com>
2023-10-20 11:11:58 -0500

    Added in more cors/header settings | copied from babemon api

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 
 app.use(cors())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -110,7 +116,7 @@ const sliceOverSizedFile = async (oversizeOutputFile, filename, res) => {
     fs.mkdirSync(outputDirectory)
   }
 
-  const sliceSize = 15000000 // 25MB
+  const sliceSize = 15000000 // 15MB
   const sliceCount = Math.ceil(oversizeOutputFile.length / sliceSize)
 
   for (let i = 0; i < sliceCount; i++) {
